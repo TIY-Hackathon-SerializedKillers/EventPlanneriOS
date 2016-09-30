a0d51c11a1d8d9eb3469f5909605dec9ec9c22e8
Dan Esrey <danesrey@dans-air.lan>
2016-09-29 21:36:33 -0400

    initial commit

--- HackAThon/MainMenuViewController.swift
+++ HackAThon/MainMenuViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainMenuViewController.swift
 //  HackAThon
 //
 //  Created by Dan Esrey on 2016/29/09.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainMenuViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
